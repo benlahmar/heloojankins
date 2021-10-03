ecb4ada0d80b1376a02fa17ecb250a734a042de9
moi <moi@moi-PC>
2021-10-03 01:12:58 +0100

    add

---
+++ habib.ben/src/main/java/habib/ben/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package habib.ben;
+
+/**
+ * @author BEN LAHMAR
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("ello habib");
+	}
+}
